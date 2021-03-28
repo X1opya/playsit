bfee2d07c688d057e1967248457e9cf7be5101fc
X1opya <x1opya@gmail.com>
2021-03-28 13:10:56 +0500

    reindex

--- app/src/main/java/dev/playsit/model/Similar.kt
+++
@@ -1,8 +0,0 @@
-package dev.playsit.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Similar(
-    @SerializedName("similar")
-    val similar: List<FeedItem>
-)
--- app/src/main/java/dev/playsit/ui/AppRoute.kt
+++
@@ -1,7 +0,0 @@
-package dev.playsit.ui
-
-sealed class AppRoute {
-    object SplashRoute : AppRoute()
-    object CollectionsRout : AppRoute()
-    object BoardsRout : AppRoute()
-}
